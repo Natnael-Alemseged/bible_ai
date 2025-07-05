1a3dab8e0549c741d0c8cb4d5eaa20c58614d265
Natnael-Alemseged <natiaabaydam@gmail.com>
2025-07-05 17:31:03 +0300

    ios imessage loader added

--- lib/ui/views/chat/chat_messages.dart
+++ lib/ui/views/chat/chat_messages.dart
@@ -21,32 +21,9 @@ class ChatMessages extends StatelessWidget {
         padding: const EdgeInsets.symmetric(vertical: 12),
         itemBuilder: (context, index) {
           if (index == messages.length && showTypingIndicator) {
-            return Align(
+            return const Align(
               alignment: Alignment.centerLeft,
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                margin: const EdgeInsets.symmetric(vertical: 4),
-                padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: const Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SizedBox(
-                      width: 10,
-                      height: 10,
-                      child: CircularProgressIndicator(
-                        strokeWidth: 2,
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
-                      ),
-                    ),
-                    SizedBox(width: 8),
-                    Text("Typing...", style: TextStyle(color: Colors.black)),
-                  ],
-                ),
-              ),
+              child: TypingIndicatorBubble(),
             );
           }
 
@@ -69,6 +46,88 @@ class ChatMessages extends StatelessWidget {
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class TypingIndicatorBubble extends StatefulWidget {
+  const TypingIndicatorBubble({super.key});
+
+  @override
+  State<TypingIndicatorBubble> createState() => _TypingIndicatorBubbleState();
+}
+
+class _TypingIndicatorBubbleState extends State<TypingIndicatorBubble> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> dotOneAnim;
+  late Animation<double> dotTwoAnim;
+  late Animation<double> dotThreeAnim;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1200),
+      vsync: this,
+    )..repeat();
+
+    dotOneAnim = Tween<double>(begin: 0, end: -3).animate(
+      CurvedAnimation(parent: _controller, curve: const Interval(0.0, 0.6, curve: Curves.easeInOut)),
+    );
+    dotTwoAnim = Tween<double>(begin: 0, end: -3).animate(
+      CurvedAnimation(parent: _controller, curve: const Interval(0.2, 0.8, curve: Curves.easeInOut)),
+    );
+    dotThreeAnim = Tween<double>(begin: 0, end: -3).animate(
+      CurvedAnimation(parent: _controller, curve: const Interval(0.4, 1.0, curve: Curves.easeInOut)),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _buildDot(Animation<double> animation) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(0, animation.value),
+          child: child,
+        );
+      },
+      child: Container(
+        width: 6,
+        height: 6,
+        margin: const EdgeInsets.symmetric(horizontal: 2),
+        decoration: const BoxDecoration(
+          color: Colors.black,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildDot(dotOneAnim),
+          _buildDot(dotTwoAnim),
+          _buildDot(dotThreeAnim),
+        ],
       ),
     );
   }
